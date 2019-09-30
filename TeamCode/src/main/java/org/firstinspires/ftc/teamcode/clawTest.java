1dde427eb23b46a494e7f450602c0b3fcf4d00df
zanzan13531 <zanzan13531@gmail.com>
2019-09-29 19:31:54 -0700

    created clawtest, it's still just a copy of armtest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//Test
+//Hi
+@TeleOp(name = "Arm Test", group = "Experimental")
+public class clawTest extends LinearOpMode {
+    Hardware robot = new Hardware();
+    driveBase drive = new driveBase(robot);
+
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            drive.drive(gamepad1, telemetry);
+
+            robot.arm.setPower(gamepad1.right_stick_y*0.2); //added a multiplier of 0.2
+
+            telemetry.addData("MS_KOL", robot.arm.getPower());
+
+            telemetry.update();
+            sleep(25);
+        }
+    }
+}
