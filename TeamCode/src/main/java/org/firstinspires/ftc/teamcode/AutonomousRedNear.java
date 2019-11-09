1965fd8491c45fa44e8a37427c7cf8c327038f22
spacepotatoturtle <meetyourcreater@gmail.com>
2019-11-09 15:51:58 -0800

    Implemented switches into robot controller which controls the type of autonomous that is run.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedNear.java
@@ -17,12 +17,15 @@ public class AutonomousRedNear extends LinearOpMode{
     public void runOpMode() {
         robot.init(hardwareMap);
 
-//        Context context = hardwareMap.appContext;
-//        int id_p = context.getResources().getIdentifier("platform", "id", context.getPackageName());
-//        Switch platform = (((Activity)context).findViewById(id_p));
-//
-//        int id_c = context.getResources().getIdentifier("color", "id", context.getPackageName());
-//        Switch color = (((Activity)context).findViewById(id_c));
+        Context context = hardwareMap.appContext;
+        int id_p = context.getResources().getIdentifier("platform", "id", context.getPackageName());
+        Switch platform = (((Activity)context).findViewById(id_p));
+
+        int id_c = context.getResources().getIdentifier("color", "id", context.getPackageName());
+        Switch color = (((Activity)context).findViewById(id_c));
+
+        int id_t = context.getResources().getIdentifier("team", "id", context.getPackageName());
+        Switch team = (((Activity)context).findViewById(id_t));
 
         telemetry.addData(">", "Press Play to start");
         telemetry.update();
@@ -47,8 +50,34 @@ public class AutonomousRedNear extends LinearOpMode{
 //        sleep(100);
 //        encoderDrive.encoderDrive(0.3, "Forward", -8, 10, this, telemetry);
 
-        encoderDrive.encoderDrive(0.3, "Forward", 27, 10, this, telemetry);
-        sleep(100);
-        encoderDrive.encoderDrive(0.3, "Strafe", -27, 10, this, telemetry);
+        if (team.isChecked()) {
+            if (color.isChecked()) {
+                if (platform.isChecked()) {
+                    encoderDrive.encoderDrive(0.3, "Forward", 27, 10, this, telemetry);
+                    sleep(100);
+                    encoderDrive.encoderDrive(0.3, "Strafe", -27, 10, this, telemetry);
+                } else {
+                    encoderDrive.encoderDrive(0.3, "Forward", 27, 10, this, telemetry);
+                    sleep(100);
+                    encoderDrive.encoderDrive(0.3, "Strafe", 27, 10, this, telemetry);
+                }
+            } else {
+                if (platform.isChecked()) {
+                    encoderDrive.encoderDrive(0.3, "Forward", 27, 10, this, telemetry);
+                    sleep(100);
+                    encoderDrive.encoderDrive(0.3, "Strafe", 27, 10, this, telemetry);
+                } else {
+                    encoderDrive.encoderDrive(0.3, "Forward", 27, 10, this, telemetry);
+                    sleep(100);
+                    encoderDrive.encoderDrive(0.3, "Strafe", -27, 10, this, telemetry);
+                }
+            }
+        } else {
+            encoderDrive.encoderDrive(0.3, "Forward", 54, 10, this, telemetry);
+            sleep(20000);
+            encoderDrive.encoderDrive(0.3, "Forward", -30, 10, this, telemetry);
+            sleep(100);
+            encoderDrive.encoderDrive(0.3, "Strafe", 18, 10, this, telemetry);
+        }
     }
 }
