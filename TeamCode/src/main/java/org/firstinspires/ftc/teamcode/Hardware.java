4b26343917062d4ab1d568b2d9a8ba46f46e5bda
zanzan13531 <zanzan13531@gmail.com>
2019-12-18 21:47:27 -0800

    added testing program, removed unecessary hardware from hardware.java
    removed a couple of servos and the color sensor because we won't be using them anymore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -17,19 +17,19 @@ public class Hardware {
     public Servo claw = null;
 
     public DcMotor arm = null;
-    public CRServo rise = null;
+    //public CRServo rise = null;
 
     public Servo baseL = null;
     public Servo baseR = null;
 
-    //public Servo autoblueArm = null;
-    //public Servo autoblueClaw = null;
+    public Servo autoblueArm = null;
+    public Servo autoblueClaw = null;
 
     public BNO055IMU imu = null;
-    public ColorSensor color = null;
+    //public ColorSensor color = null;
 
-    public Servo dArm = null;
-    public CRServo dClaw = null;
+    //public Servo dArm = null;
+    //public CRServo dClaw = null;
 
 
     HardwareMap hwMap = null;
@@ -52,15 +52,15 @@ public class Hardware {
 
         claw                = hwMap.get(Servo.class,        "CL");
         arm                 = hwMap.get(DcMotor.class,      "AR");
-        rise                = hwMap.get(CRServo.class,      "RS");
+        //rise                = hwMap.get(CRServo.class,      "RS");
         baseL               = hwMap.get(Servo.class,        "BL");
         baseR               = hwMap.get(Servo.class,        "BR");
-        dArm                = hwMap.get(Servo.class,        "DA");
-        dClaw               = hwMap.get(CRServo.class,      "DC");
+        //dArm                = hwMap.get(Servo.class,        "DA");
+        //dClaw               = hwMap.get(CRServo.class,      "DC");
         imu                 = hwMap.get(BNO055IMU.class,    "IM");
-        color               = hwMap.get(ColorSensor.class,  "CS");
-        //autoblueArm         = hwMap.get(Servo.class,        "BA");
-        //autoblueClaw        = hwMap.get(Servo.class,        "BC");
+        //color               = hwMap.get(ColorSensor.class,  "CS");
+        autoblueArm         = hwMap.get(Servo.class,        "BA");
+        autoblueClaw        = hwMap.get(Servo.class,        "BC");
 
         frontLeftDrive. setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -70,18 +70,21 @@ public class Hardware {
         arm.setDirection(DcMotor.Direction.FORWARD);
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        rise.setDirection(CRServo.Direction.FORWARD);
-        rise.setPower(0);
+        //rise.setDirection(CRServo.Direction.FORWARD);
+        //rise.setPower(0);
 
         claw.setPosition(0);
 
         baseL.setPosition(0);
         baseR.setPosition(1);
 
-        dArm.setPosition(1);
+        //dArm.setPosition(1);
 
-        dClaw.setDirection(CRServo.Direction.FORWARD);
-        dClaw.setPower(0);
+        //dClaw.setDirection(CRServo.Direction.FORWARD);
+        //dClaw.setPower(0);
+
+        autoblueArm.setPosition(0.5);
+        autoblueClaw.setPosition(0.5);
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
